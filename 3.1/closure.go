14019b1441fe5e5fd3670d3858d5e3810a122b9c
sugawara kensuke <superradio86@gmail.com>
2022-04-19 21:42:26 +0900

    add example for memory usage of gorutine

--- 3.1/closure.go
+++ 3.1/closure.go
@@ -15,4 +15,22 @@ func main() {
 	}()
 	wg.Wait()
 	fmt.Println(salutation)
+
+	for _, salutation := range []string{"hello", "greetings", "good day"} {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Println(salutation)
+		}()
+	}
+	wg.Wait()
+
+	for _, salutation := range []string{"hello", "greetings", "good day"} {
+		wg.Add(1)
+		go func(salutation string) {
+			defer wg.Done()
+			fmt.Println(salutation)
+		}(salutation)
+	}
+	wg.Wait()
 }
