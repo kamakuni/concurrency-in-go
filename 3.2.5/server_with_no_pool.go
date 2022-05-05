ff109a5dd2a7718bfe8d8ad36790988eb2a2d408
sugawara kensuke <superradio86@gmail.com>
2022-05-05 17:40:53 +0900

    add a function for benchmark

--- 3.2.5/server_with_no_pool.go
+++ 3.2.5/server_with_no_pool.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"sync"
+	"testing"
 	"time"
 )
 
@@ -42,4 +44,17 @@ func startNetworkDaemon() *sync.WaitGroup {
 func init() {
 	daemonStarted := startNetworkDaemon()
 	daemonStarted.Wait()
+}
+
+func BenchmarkNetworkRequest(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		conn, err := net.Dial("tcp", "localhost:8080")
+		if err != nil {
+			b.Fatalf("cannot dial host: %v", err)
+		}
+		if _, err := ioutil.ReadAll(conn); err != nil {
+			b.Fatalf("cannot read: %v", err)
+		}
+		conn.Close()
+	}
 }
