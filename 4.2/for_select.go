6d179c8efe4eee6d9b6ab67ab080d1e2bd46aa92
sugawara kensuke <superradio86@gmail.com>
2022-05-27 22:15:03 +0900

    update example for select with for

--- 4.2/for_select.go
+++ 4.2/for_select.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
 func doSomething(done chan interface{}, stringStream chan string) {
-	defer close(done)
 	for _, s := range []string{"a", "b", "c"} {
 		select {
 		case <-done:
 			fmt.Println("done")
 			return
 		case stringStream <- s:
-			fmt.Println(s)
 		}
 	}
 	return
@@ -23,11 +20,10 @@ func main() {
 	done := make(chan interface{})
 	stringStream := make(chan string)
 
-	var wg sync.WaitGroup
-	wg.Add(1)
 	go doSomething(done, stringStream)
-	select {
-	case s := <-stringStream:
-		fmt.Println(s)
-	}
+
+	fmt.Println(<-stringStream)
+	fmt.Println(<-stringStream)
+	fmt.Println(<-stringStream)
+
 }
