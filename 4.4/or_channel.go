0e69412634ba391fecd7d72909387d03425f55ec
sugawara kensuke <superradio86@gmail.com>
2022-06-02 23:03:31 +0900

    update example to or channel

--- 4.4/or_channel.go
+++ 4.4/or_channel.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 	var or func(channels ...<-chan interface{}) <-chan interface{}
 	or = func(channels ...<-chan interface{}) <-chan interface{} {
@@ -30,4 +35,22 @@ func main() {
 		}()
 		return orDone
 	}
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+	fmt.Printf("done after %v", time.Since(start))
 }
